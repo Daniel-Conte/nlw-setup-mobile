f99c12f4cd116bd3ce44d1da858fa8ed273cc377
Daniel Conte <182736@upf.br>
2023-01-19 18:32:46 -0300

    cria layout de habito e novo habito

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native";
 import { Text, View, ScrollView } from "react-native";
 import HabitDay, { DAY_SIZE } from "../components/HabitDay";
 import Header from "../components/Header";
@@ -10,6 +11,8 @@ const minimumSummaryDatesSizes = 18 * 5;
 const amountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearStart.length;
 
 function Home() {
+  const { navigate } = useNavigation();
+
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <Header />
@@ -32,7 +35,10 @@ function Home() {
       >
         <View className="flex-row flex-wrap">
           {datesFromYearStart.map((date) => (
-            <HabitDay key={date.toISOString()} />
+            <HabitDay
+              key={date.toISOString()}
+              onPress={() => navigate("habit", { date: date.toISOString() })}
+            />
           ))}
 
           {amountOfDaysToFill > 0 &&
