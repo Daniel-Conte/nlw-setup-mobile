ae165f2484618f799e20096e24fbcffbcc5dde09
Daniel Conte <182736@upf.br>
2023-01-27 12:07:41 -0300

    configura push notifications

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import "./src/lib/dayjs";
-import { StatusBar } from "react-native";
+import { Button, StatusBar } from "react-native";
+import * as Notifications from "expo-notifications";
 import {
   useFonts,
   Inter_400Regular,
@@ -10,6 +11,14 @@ import {
 import Loading from "./src/components/Loading";
 import Routes from "./src/routes";
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
 export default function App() {
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
@@ -18,10 +27,40 @@ export default function App() {
     Inter_800ExtraBold,
   });
 
+  async function scheduleNotification() {
+    const trigger = new Date(Date.now());
+    trigger.setMinutes(trigger.getMinutes() + 1);
+
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "Habits 🤩",
+        body: "Você praticou seus hábitos hoje?",
+      },
+      trigger,
+    });
+  }
+
+  async function getScheduledNotifications() {
+    const schedules = await Notifications.getAllScheduledNotificationsAsync();
+
+    console.log(schedules);
+  }
+
+  async function cancelScheduledNotification() {
+    const schedules = await Notifications.getAllScheduledNotificationsAsync();
+
+    await Notifications.cancelScheduledNotificationAsync(
+      schedules[0].identifier
+    );
+  }
+
   if (!fontsLoaded) return <Loading />;
 
   return (
     <>
+      {/* <Button title="Enviar notificação" onPress={scheduleNotification} />
+      <Button title="Agendadas" onPress={getScheduledNotifications} />
+      <Button title="Cancelar" onPress={cancelScheduledNotification} /> */}
       <Routes />
       <StatusBar barStyle="light-content" translucent />
     </>
